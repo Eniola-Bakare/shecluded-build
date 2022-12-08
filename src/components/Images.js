cf826455b1b262a2003538d90445f7e27da38b7a
Onyeka Nnabigwu <onyekannabugwu>
2022-12-08 10:55:29 +0100

    Making the home page responsive

--- src/components/Images.js
+++ src/components/Images.js
@@ -4,5 +4,9 @@ import image3 from '../assets/imageSlider/image3.png'
 import image4 from '../assets/imageSlider/image4.png'
 import image5 from '../assets/imageSlider/image5.png'
 import image6 from '../assets/imageSlider/image6.png'
+import image7 from '../assets/imageSlider/image5.png'
+import image8 from '../assets/imageSlider/image2.png'
+import image9 from '../assets/imageSlider/image3.png'
+import image10 from '../assets/imageSlider/image4.png'
 
-export default [image1, image2, image3,  image4, image5, image6]
+export default [image1, image2, image3,  image4, image5, image6, image7, image8, image9, image10]
